ecff75a7fa4157e96eb089db8dd980d4c1ba870d
Matthew Bock <mattb93@vt.edu>
2012-10-31 00:26:36 -0400

    First Commit
    Let's hope this works.

---
+++ War/src/groupproject/wargame/LaunchScreen.java
@@ -0,0 +1,17 @@
+package groupproject.wargame;
+
+import sofia.app.ShapeScreen;
+
+// -------------------------------------------------------------------------
+/**
+ * Write a one-sentence summary of your class here. Follow it with additional
+ * details about its purpose, what abstraction it represents, and how to use it.
+ *
+ * @author Matt
+ * @version Oct 31, 2012
+ */
+public class LaunchScreen
+    extends ShapeScreen
+{
+    // TODO: stuff
+}
