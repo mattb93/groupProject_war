1659844f6b540d277a284df76cc5cf457df94a10
Sonika <Sonika@10.252.15.180>
2012-11-23 22:31:36 -0800

    deck

---
+++ War/src/groupproject/wargame/Deck.java
@@ -0,0 +1,123 @@
+package groupproject.wargame;
+
+import java.util.EmptyStackException;
+import java.util.Random;
+import java.util.ArrayList;
+
+// -------------------------------------------------------------------------
+/**
+ * Constructs Deck of cards for the player
+ *
+ * @author Matthew Bock (mattb93)
+ * @author Kevin Olson (kevino93)
+ * @author John Sebestin (jseb11)
+ * @author Sonika Singh (sonika06)
+ * @version Nov 14, 2012
+ */
+
+public class Deck
+{
+
+    /**
+     * An Array List of type Card
+     */
+    ArrayList<Card> decks;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Deck object.
+     */
+    public Deck()
+    {
+        decks = new ArrayList<Card>();
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * It adds the card at end of the pile.
+     *
+     * @param card
+     *            the card to put on the stack
+     */
+    public void add(Card card)
+    {
+        decks.add(card);
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * It removes and returns the top card.
+     *
+     * @return the card at the top
+     */
+    public Card top()
+
+    {
+        if (decks.size() == 0)
+        {
+            throw new EmptyStackException();
+        }
+        else
+        {
+            return decks.remove(decks.size() - 1);
+        }
+
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gives size of the deck.
+     *
+     * @return size of the deck
+     */
+    public int size()
+
+    {
+        return decks.size();
+
+    }
+
+
+    /**
+     * Randomly shuffles the pile .removes the top card and puts another card to
+     * that position
+     */
+    public void shuffle()
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < decks.size(); i++)
+
+        {
+
+            if (size() > 0)
+            {
+
+                Card top = decks.remove(decks.size() - 1);
+                int now = rand.nextInt(decks.size());
+
+                decks.add(now, top);
+
+            }
+
+        }
+
+    }
+
+
+    /**
+     * It clears the deck
+     */
+    public void clear()
+
+    {
+
+        decks.clear();
+
+    }
+
+}
---
+++ War/src/groupproject/wargame/DeckTest.java
@@ -0,0 +1,46 @@
+package groupproject.wargame;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import student.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+// -------------------------------------------------------------------------
+/**
+ * Tests for the Deck class
+ *
+ * @author Matthew Bock (mattb93)
+ * @author Kevin Olson (kevino93)
+ * @author John Sebestin (jseb11)
+ * @author Sonika Singh (sonika06)
+ * @version Nov 14, 2012
+ */
+
+public class DeckTest
+    extends TestCase
+{
+    private ArrayList<Card> decks1;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Creates a brand new empty stack for test.
+     */
+
+    public void setUp()
+
+    {
+        decks1 = new ArrayList<Card>();
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Tests The top method.
+     */
+    public void testTop()
+    {
+
+    }
+}
