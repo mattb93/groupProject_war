2b3b180b380a016144b2b597f4a9dd16300b37a7
Matthew Bock <mattb93@vt.edu>
2012-11-26 21:32:33 -0500

    CardPlayer and CardGame abstract classes.

---
+++ War/src/groupproject/wargame/CardGame.java
@@ -0,0 +1,49 @@
+package groupproject.wargame;
+
+// -------------------------------------------------------------------------
+/**
+ * The basic logic for card games. Different games with different sets of rules
+ * will extend this one.
+ *
+ * @author Matthew Bock (mattb93)
+ * @author Kevin Olson (kevino93)
+ * @author John Sebestin (jseb11)
+ * @author Sonika Singh (sonika06)
+ * @version Nov 26, 2012
+ */
+public abstract class CardGame
+{
+    private Deck       deck = new Deck();
+    private CardPlayer player1;
+    private CardPlayer player2;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Fills and shuffles the deck.
+     */
+    public void prepareCards()
+    {
+        deck.fill();
+        deck.shuffle();
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Resets the deck, then fills and shuffles it again.
+     */
+    public void resetDeck()
+    {
+        deck = new Deck();
+        deck.fill();
+        deck.shuffle();
+    }
+
+
+    public void setPlayers(CardPlayer firstPlayer, CardPlayer secondPlayer)
+    {
+        player1 = firstPlayer;
+        player2 = secondPlayer;
+    }
+}
---
+++ War/src/groupproject/wargame/CardPlayer.java
@@ -0,0 +1,56 @@
+package groupproject.wargame;
+
+import java.util.ArrayList;
+
+// -------------------------------------------------------------------------
+/**
+ * This class contains the basic logic that will be used by players regardless
+ * of which card game they are playing. As we add game modes, this class will be
+ * exended by the individual games' player classes.
+ *
+ * @author Matthew Bock (mattb93)
+ * @author Kevin Olson (kevino93)
+ * @author John Sebestin (jseb11)
+ * @author Sonika Singh (sonika06)
+ * @version Nov 26, 2012
+ */
+public abstract class CardPlayer
+{
+    private ArrayList<Card> hand = new ArrayList<Card>(26);
+
+
+    // ----------------------------------------------------------
+    /**
+     * Add's a card to the player's hand.
+     *
+     * @param card
+     *            the card to add to the hand.
+     */
+    public void receiveCard(Card card)
+    {
+        hand.add(card);
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Removes and returns the n-th card in the player's hand.
+     *
+     * @param n
+     *            the index of the card for the player to give.
+     * @return the card that was removed.
+     */
+    public Card giveCard(int n)
+    {
+        return hand.remove(n);
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Empties the player's hand.
+     */
+    public void clearHand()
+    {
+        hand = new ArrayList<Card>(26);
+    }
+}
