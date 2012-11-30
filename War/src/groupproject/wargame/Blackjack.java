3304bc1bc524589261b351f963a38178f37cf1eb
vin Olson <kevino93@vt.edu>
2012-11-30 16:24:02 -0500

    Hi

--- War/src/groupproject/wargame/Blackjack.java
+++ War/src/groupproject/wargame/Blackjack.java
@@ -1,7 +1,32 @@
 package groupproject.wargame;
 
+// -------------------------------------------------------------------------
+/**
+ * Blackjack game class
+ *
+ * @author Kevin Olson (kevino93)
+ * @author Matthew Bock (mattb93)
+ * @author John Sebestin (jseb11)
+ * @author Sonika Singh (sonika06)
+ * @version Nov 29, 2012
+ */
 public class Blackjack
 {
     private Deck deck;
+    private CardPlayer player;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Creates a deck with cards and then shuffles them
+     */
+    public void setUp()
+    {
+        deck = new Deck();
+        deck.fill();
+        deck.shuffle();
+    }
+
+
 
 }
