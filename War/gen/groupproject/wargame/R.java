e57260d870bc970709f95a33d07a7099fe240e0c
Matthew Bock <mattb93@vt.edu>
2012-12-04 21:47:26 -0500

    Tonight's Work.

--- War/gen/groupproject/wargame/R.java
+++ War/gen/groupproject/wargame/R.java
@@ -10,6 +10,9 @@ package groupproject.wargame;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int green=0x7f060000;
+    }
     public static final class drawable {
         public static final int cardspace=0x7f020000;
         public static final int clubs10=0x7f020001;
@@ -68,25 +71,26 @@ public final class R {
         public static final int spades9=0x7f020036;
     }
     public static final class id {
-        public static final int blackJackButton=0x7f070009;
-        public static final int hitButton=0x7f070001;
-        public static final int menu_settings=0x7f07000b;
-        public static final int shapeView=0x7f070000;
-        public static final int standButton=0x7f070002;
-        public static final int textView1=0x7f070008;
-        public static final int textView2=0x7f070007;
-        public static final int textView3=0x7f070006;
-        public static final int textView4=0x7f070005;
-        public static final int textView5=0x7f070004;
-        public static final int textView6=0x7f070003;
-        public static final int warButton=0x7f07000a;
+        public static final int blackJackButton=0x7f08000a;
+        public static final int hitButton=0x7f080002;
+        public static final int menu_settings=0x7f08000c;
+        public static final int resetButton=0x7f080000;
+        public static final int shapeView=0x7f080001;
+        public static final int standButton=0x7f080003;
+        public static final int textView1=0x7f080009;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f080007;
+        public static final int textView4=0x7f080006;
+        public static final int textView5=0x7f080005;
+        public static final int textView6=0x7f080004;
+        public static final int warButton=0x7f08000b;
     }
     public static final class layout {
         public static final int blackjackscreen=0x7f030000;
         public static final int launchscreen=0x7f030001;
     }
     public static final class menu {
-        public static final int launchscreen=0x7f060000;
+        public static final int launchscreen=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- War/src/groupproject/wargame/BlackJackScreen.java
+++ War/src/groupproject/wargame/BlackJackScreen.java
@@ -1,5 +1,8 @@
 package groupproject.wargame;
 
+import sofia.graphics.TextShape;
+import sofia.graphics.RectangleShape;
+import android.widget.Toast;
 import java.util.ArrayList;
 import android.graphics.RectF;
 import sofia.graphics.ImageShape;
@@ -20,15 +23,28 @@ import sofia.graphics.Color;
 public class BlackJackScreen
     extends ShapeScreen
 {
-    private Blackjack game;
-    private float     height;
-    private float     width;
-    private float     cardHeight;
-    private float     cardWidth;
+    private Blackjack             game;
+
+    private float                 height;
+    private float                 width;
+    private float                 cardHeight;
+    private float                 cardWidth;
+
     private ArrayList<ImageShape> dealerCardList;
-    private int nextDealerIndex = 0;
+    private int                   nextDealerIndex;
     private ArrayList<ImageShape> playerCardList;
-    private int nextPlayerIndex = 0;
+    private int                   nextPlayerIndex;
+
+    private boolean               hitable;
+    private boolean               standable;
+
+    private RectangleShape        dealerBox;
+    private TextShape             dealerName;
+    private TextShape             dealerPointsShape;
+
+    private RectangleShape        playerBox;
+    private TextShape             playerName;
+    private TextShape             playerPointsShape;
 
 
     // ----------------------------------------------------------
@@ -38,6 +54,10 @@ public class BlackJackScreen
     public void initialize()
     {
         game = new Blackjack();
+        hitable = true;
+        standable = true;
+        nextDealerIndex = 0;
+        nextPlayerIndex = 0;
 
         dealerCardList = new ArrayList<ImageShape>(8);
         playerCardList = new ArrayList<ImageShape>(8);
@@ -50,7 +70,7 @@ public class BlackJackScreen
 
         for (float i = 0; i < 2 * cardHeight; i = i + cardHeight)
         {
-            for (float j = 0; j < cardWidth*4; j = j + cardWidth)
+            for (float j = 0; j < cardWidth * 4; j = j + cardWidth)
             {
                 RectF rect = new RectF(0.0f, 0.0f, cardWidth, cardHeight);
                 ImageShape topCardSpace = new ImageShape("cardspace", rect);
@@ -60,9 +80,9 @@ public class BlackJackScreen
             }
         }
 
-        for (float i = height - 2*cardHeight; i < height; i = i + cardHeight)
+        for (float i = height - 2 * cardHeight; i < height; i = i + cardHeight)
         {
-            for (float j = 0; j <cardWidth*4; j = j + cardWidth)
+            for (float j = 0; j < cardWidth * 4; j = j + cardWidth)
             {
                 RectF rect = new RectF(0.0f, 0.0f, cardWidth, cardHeight);
                 ImageShape bottomCardSpace = new ImageShape("cardspace", rect);
@@ -71,7 +91,66 @@ public class BlackJackScreen
                 add(bottomCardSpace);
             }
         }
+
+        dealerBox = new RectangleShape(4 * cardWidth, 0, width, 2 * cardHeight);
+        dealerBox.setColor(Color.black);
+        dealerBox.setFillColor(Color.green);
+        dealerBox.setFilled(true);
+        add(dealerBox);
+
+        dealerName = new TextShape("Dealer", 4 * cardWidth + 5, 5);
+        dealerName.setColor(Color.black);
+        dealerName.setTypeSize(5f);
+        add(dealerName);
+
+        dealerPointsShape =
+            new TextShape("" + 0, 4 * cardWidth + 5, cardHeight / 2);
+        dealerPointsShape.setColor(Color.black);
+        dealerPointsShape.setTypeSize(8f);
+        add(dealerPointsShape);
+
+        playerBox =
+            new RectangleShape(
+                4 * cardWidth,
+                height - 2 * cardHeight,
+                width,
+                height);
+        playerBox.setColor(Color.red);
+        playerBox.setFillColor(Color.green);
+        playerBox.setFilled(true);
+        add(playerBox);
+
+        playerName =
+            new TextShape("You", 4 * cardWidth + 5, height - 2*cardHeight + 5);
+        playerName.setColor(Color.black);
+        playerName.setTypeSize(5f);
+        add(playerName);
+
+        playerPointsShape =
+            new TextShape("" + 0, 4 * cardWidth + 5, height
+                - cardHeight);
+        playerPointsShape.setColor(Color.black);
+        playerPointsShape.setTypeSize(8f);
+        add(playerPointsShape);
+
+        game.dealHand();
+
+        for (Card c : game.returnPlayer().returnHand())
+        {
+            playerCardList.get(nextPlayerIndex).setImage(c.toGraphic());
+            nextPlayerIndex++;
+        }
+
+        for (Card c : game.returnDealer().returnHand())
+        {
+            dealerCardList.get(nextDealerIndex).setImage(c.toGraphic());
+            nextDealerIndex++;
+        }
+
+        dealerPointsShape.setText("" + game.returnDealer().returnTotal());
+        playerPointsShape.setText("" + game.returnPlayer().returnTotal());
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -79,14 +158,79 @@ public class BlackJackScreen
      */
     public void hitButtonClicked()
     {
-        Card hit = game.hit("Player");
-        playerCardList.get(nextPlayerIndex).setImage(hit.toGraphic());
-        nextPlayerIndex++;
-        if(game.checkLose())
+        if (hitable)
         {
-            playerLost();
+            Card hit = game.hit("Player");
+
+
+            if (nextPlayerIndex > 7)
+            {
+                nextPlayerIndex = 0;
+            }
+
+            playerCardList.get(nextPlayerIndex).setImage(hit.toGraphic());
+            nextPlayerIndex++;
+
+            if (game.checkLose())
+            {
+                playerPointsShape.setText("" + game.returnPlayer().returnTotal());
+                playerLost();
+            }
+
+            playerPointsShape.setText("" + game.returnPlayer().returnTotal());
         }
     }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Called when the reset button is clicked, resets the whole screen.
+     */
+    public void resetButtonClicked()
+    {
+        initialize();
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Called when the player stands.
+     */
+    public void standButtonClicked()
+    {
+        if (standable)
+        {
+            hitable = false;
+            standable = false;
+
+            Boolean result = game.playerStand();
+
+            dealerPointsShape.setText("" + game.returnDealer().returnTotal());
+
+            if (result)
+            {
+                nextDealerIndex = 0;
+                for (Card c : game.returnDealer().returnHand())
+                {
+                    dealerCardList.get(nextDealerIndex).setImage(c.toGraphic());
+                    nextDealerIndex++;
+                }
+                playerLost();
+            }
+
+            if (!result)
+            {
+                nextDealerIndex = 0;
+                for (Card c : game.returnDealer().returnHand())
+                {
+                    dealerCardList.get(nextDealerIndex).setImage(c.toGraphic());
+                    nextDealerIndex++;
+                }
+                playerWon();
+            }
+        }
+    }
+
 
     // ----------------------------------------------------------
     /**
@@ -94,8 +238,12 @@ public class BlackJackScreen
      */
     public void playerLost()
     {
-
+        hitable = false;
+        standable = false;
+        String str = "You Lost! Press reset to play again.";
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
     }
+
 
     // ----------------------------------------------------------
     /**
@@ -103,6 +251,9 @@ public class BlackJackScreen
      */
     public void playerWon()
     {
-
+        hitable = false;
+        standable = false;
+        String str = "You Won! Press reset to play again.";
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
     }
 }
